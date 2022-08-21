481cad17bd5cceb63240b3427372983fe1ceafbe
S. M. Saifullah <shakil.m.19@gmail.com>
2022-08-22 02:22:58 +0600

    added click event on cards

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
+let players = [];
 
+function addToChosenV(element) {
+  const playerName = element.parentNode.parentNode.children[0].innerText;
+
+  players.push(playerName);
+
+  displayChosenPlayers();
+
+  //   Disabling Select button after players being selected
+  element.setAttribute("disabled", "disabled");
+}
+
+function displayChosenPlayers() {
+  const ul = document.getElementById("chosen-player");
+  ul.innerHTML = "";
+
+  for (let i = 0; i < players.length; i++) {
+    const player = players[i];
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <span class="mr-3">${i + 1}. </span><span>${player}</span>
+    `;
+
+    if (ul.childElementCount > 5) {
+      alert("You cannot select more than 5 players");
+      return;
+    } else {
+      ul.appendChild(li);
+    }
+    li.classList.add("mb-5");
+  }
+}
