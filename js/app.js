250ebca3584eba32dc7bfc928dec331fe7e4fa63
S. M. Saifullah <shakil.m.19@gmail.com>
2022-08-22 12:31:24 +0600

    added more cards and error handling scripts

--- js/app.js
+++ js/app.js
@@ -2,18 +2,19 @@ let players = [];
 
 function addToChosenV(element) {
   const playerName = element.parentNode.parentNode.children[0].innerText;
-
-  players.push(playerName);
-
-  displayChosenPlayers();
-
   //   Disabling Select button after players being selected
-  element.setAttribute("disabled", "disabled");
+  if (players.length < 5) {
+    players.push(playerName);
+    element.setAttribute("disabled", "disabled");
+  } else if (players.length === 5) {
+    alert("You cannot select more than 5 players.");
+  }
+  displayChosenPlayers();
 }
 
 function displayChosenPlayers() {
-  const ul = document.getElementById("chosen-player");
-  ul.innerHTML = "";
+  const ol = document.getElementById("chosen-player");
+  ol.innerHTML = "";
 
   for (let i = 0; i < players.length; i++) {
     const player = players[i];
@@ -21,13 +22,44 @@ function displayChosenPlayers() {
     li.innerHTML = `
         <span class="mr-3">${i + 1}. </span><span>${player}</span>
     `;
-
-    if (ul.childElementCount > 5) {
-      alert("You cannot select more than 5 players");
-      return;
-    } else {
-      ul.appendChild(li);
-    }
+    ol.appendChild(li);
     li.classList.add("mb-5");
   }
 }
+
+function calculateExpense() {
+  const numberOfPlayers = players.length;
+  const expensePerPlayer = getElementValueById("per-player-cost");
+
+  if (isNaN(expensePerPlayer)) {
+    alert("Please Enter a Valid Amount For Each Player");
+    return;
+  } else {
+    const expenseTotalPlayer = numberOfPlayers * expensePerPlayer;
+    setTextById("player-total-expense", expenseTotalPlayer);
+  }
+}
+
+document
+  .getElementById("calculate-player-expense")
+  .addEventListener("click", calculateExpense);
+
+document
+  .getElementById("calculate-total-expense")
+  .addEventListener("click", function () {
+    const expenseManager = getElementValueById("expense-manager");
+    const expenseCoach = getElementValueById("expense-coach");
+    calculateExpense();
+    const expenseTotalPlayer = getElementTextById("player-total-expense");
+
+    if (isNaN(expenseManager)) {
+      alert("Please Enter a Valid Amount For Manager");
+      return;
+    } else if (isNaN(expenseCoach)) {
+      alert("Please Enter a Valid Amount For Coach");
+      return;
+    } else {
+      const expenseTotal = expenseTotalPlayer + expenseManager + expenseCoach;
+      setTextById("expense-total", expenseTotal);
+    }
+  });
